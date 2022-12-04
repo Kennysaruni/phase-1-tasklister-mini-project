33f73fa5e0034ebdfae34a8bb56977a86965a512
Kennysaruni <kenny.saruni@student.moringaschool.com>
2022-12-04 12:38:44 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener("DOMContentLoaded",  () => {
+    const form = document.getElementById("create-task-form");
+    form.addEventListener("submit", addTask);
+  });
+
+   function addTask (e) {
+    e.preventDefault();
+    const newTaskDescription = document.getElementById("new-task-description");
+    const newToDo = document.createElement("li");
+    newToDo.innerText = newTaskDescription.value;
+    appendTask(newToDo);
+    e.target.reset();
+  };
+  function appendTask (task){
+    document.getElementById("tasks").appendChild(task);
+  };
+
+  
